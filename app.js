a7ef31b783c7a5f73279050a566b6835f18ea4df
Katrin Alimova <alimova.katrin8@gmail.com>
2020-07-02 09:23:46 +0300

    initial

---
+++ app.js
@@ -0,0 +1,18 @@
+// Obtain configured instance.
+let terminal = new BluetoothTerminal();
+
+// Override `receive` method to handle incoming data as you want.
+terminal.receive = function (data) {
+  alert(data);
+};
+
+// Request the device for connection and get its name after successful connection.
+terminal.connect().then(() => {
+  alert(terminal.getDeviceName() + ' is connected!');
+});
+
+// Send something to the connected device.
+terminal.send('Simon says: Hello, world!');
+
+// Disconnect from the connected device.
+terminal.disconnect();
